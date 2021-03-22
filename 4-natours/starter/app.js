389bdb7889f1f6e138a9f0123d22a4215eeaa3dd
Dugsteer <dugsteer@gmail.com>
2021-03-22 14:32:54 +0100

    End 67

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,10 +6,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MiddleWARE
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-console.log(`${__dirname}/public`);
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!😇');
